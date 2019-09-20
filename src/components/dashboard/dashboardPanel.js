e8c8d67f2d52ef9ccd2f56534dbf8515e892bda4
radonjamarinkovic <radonja.marinkovic@gmail.com>
2019-09-20 08:53:37 +0200

    new items
    Added placeholder css and html for dashboard items

--- src/components/dashboard/dashboardPanel.js
+++ src/components/dashboard/dashboardPanel.js
@@ -18,8 +18,57 @@ const DashboardPanel = () => {
     
     return (
         <div>
-            <p>Panel works</p>
-            <button onClick={a}>Get Data</button>
+            <div className="weather-panel-item">
+                <div className="weather-panel-head">
+                    <div className="panel-title">
+                        <span className="panel-delete">
+                            More
+                        </span>
+                        <h4>London</h4>
+                        <span className="panel-delete">Delete</span>
+                    </div>
+                    <div className="panel-body">
+                        <div className="img-holder"></div>
+                        <span className="temp-value">34°C</span>
+                    </div>
+                    <div className="panel-footer">
+                        <div className="panel-footer-section">
+                           <div className="panel-footer-item">
+                               <span className="pfi-title">Max temp</span>
+                               <span className="pfi-value">34°C</span>
+                           </div>
+                            <div className="panel-footer-item">
+                                <span className="pfi-title">Min temp</span>
+                                <span className="pfi-value">26°C</span>
+                            </div>
+                        </div>
+
+                        <div className="panel-footer-section-2">
+                            <button className="panel-button">More Details</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="weather-panel-footer">
+                    <h4>Additionl info</h4>
+                    <div className="weather-footer-section">
+                        <div className="weather-footer-section-item">
+                            <h4>Humidity</h4>
+                            <span className="footer-symbol">¥</span>
+                            <p>34%<span className="si-addition">%</span></p>
+                        </div>
+                        <div className="weather-footer-section-item">
+                            <h4>Pressure</h4>
+                            <span className="footer-symbol">¥</span>
+                            <p>1028<span className="si-addition">mBar</span></p>
+                        </div>
+                        <div className="weather-footer-section-item">
+                            <h4>Sunset</h4>
+                            <span className="footer-symbol">¥</span>
+                            <p>34<span className="si-addition">hours</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/helpers/helpers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const rale = () => {
+    console.log('helloworld');
+};
+
+export const helloRale = () => {
+    console.log('hello rale')
+};
+
+//export { helloworld, helloRale};
